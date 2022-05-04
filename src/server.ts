b5a7c86fcbdc37a8b78e7da69329099c42099da2
Vitor Santos <vitor93gs@gmail.com>
2022-05-04 15:26:25 -0300

    criada primeira rota e instalado nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,38 @@
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+// get - buscar informaçoes , post - cadastrar informaçoes, put - atualizar informaçoes, delete - deletar informaçoes, patch - atualizar parcialmente
 
-app.get("/users", (req, res) => {
-    return res.send("Hello World");
-})
+
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ede3875c58c924",
+      pass: "56af00e211934a"
+    }
+  });
+
+
+app.post('/feedbacks', async (req, res) => {
+	// prisma.feedback.create({
+	//    data: {type: req.body.type, comment: req.body.comment, screenshot: req.body.screenshot}
+	//
+	// ISSO VIRA ISSO ABAIXO, DESESTRUTURAÇÃO DO OBJETO, RETIRANDO O TYPE, COMMENT E SCREENSHOT DO REQ.BODY
+	// o "type: type," pode ser escrito como: "type," o "comment: comment," pode ser escrito como: "comment," e o "screenshot: screenshot" pode ser escrito como: "screenshot"
+
+	const { type, comment, screenshot } = req.body;
+	const feedback = await prisma.feedback.create({
+		data: { type, comment, screenshot },
+	});
+	res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
-    console.log("HEROU")
-})
+	console.log('HEROU');
+});
 
 // SQLite
