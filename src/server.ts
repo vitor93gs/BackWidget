5aed7d19d9dbfcafa82e93376b6ee1972853172f
Vitor Santos <vitor93gs@gmail.com>
2022-05-04 13:17:50 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/users", (req, res) => {
+    return res.send("Hello World");
+})
+
+app.listen(3333, () => {
+    console.log("HEROU")
+})
+
+// SQLite
