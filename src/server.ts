703dcc8837a33e18dbe98b1911e714c44441d31e
Vitor Santos <vitor93gs@gmail.com>
2022-05-09 15:52:44 -0300

    instalado cors e jest

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { routes } from './routes';
-
+import cors from 'cors';
 const app = express();
-app.use(express.json());
-// get - buscar informaçoes , post - cadastrar informaçoes, put - atualizar informaçoes, delete - deletar informaçoes, patch - atualizar parcialmente
 
+app.use(cors());
+app.use(express.json()); // get - buscar informaçoes , post - cadastrar informaçoes, put - atualizar informaçoes, delete - deletar informaçoes, patch - atualizar parcialmente
 app.use(routes);
 
 app.listen(3333, () => {
-	console.log('HEROU');
+	console.log('Hello World');
 });
 
 // SQLite
